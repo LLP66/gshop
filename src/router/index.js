abda29cdfa7b290c484bebc66651ea34b8ee24c5
LLP <llp@example.com>
2019-09-08 10:03:41 +0800

    init gshopgit

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+/*路由器模块*/
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Msite from "../pages/Msite/Msite";
+import Search from "../pages/Search/Search";
+import Order from "../pages/Order/Order";
+import Profile from "../pages/Profile/Profile";
+import Login from "../pages/Login/Login";
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes:[
+    {
+      path:'/',
+      redirect:'/msite'
+    },
+    {
+      path: '/msite',
+      component:Msite,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/search',
+      component:Search,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/order',
+      component:Order,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/profile',
+      component:Profile,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/login',
+      component:Login
+    }
+  ]
+})
